cccee93e3e6d58608df19605b514f97ca1c1783c
nglicloud <dattatraya.jagtap@gmail.com>
2025-04-05 21:42:46 +0530

    initial commit

---
+++ src/main/java/com/example/webapp/HelloWorld.java
@@ -0,0 +1,8 @@
+// File: src/main/java/com/example/webapp/HelloWorld.java
+package com.example.webapp;
+
+public class HelloWorld {
+    public String sayHello() {
+        return "Hello, CircleCI and AWS!";
+    }
+}
---
+++ src/test/java/com/example/webapp/HelloWorldTest.java
@@ -0,0 +1,14 @@
+// File: src/test/java/com/example/webapp/HelloWorldTest.java
+package com.example.webapp;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloWorldTest {
+
+    @Test
+    public void testSayHello() {
+        HelloWorld hw = new HelloWorld();
+        assertEquals("Hello, CircleCI and AWS!", hw.sayHello());
+    }
+}
